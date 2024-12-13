489d468657d0dbb55fa473761447ecc34f830bf2
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2024-12-14 07:51:50 +0800

    custom code on connected blynk

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,32 @@
-/*************************************************************
 
-  This is a simple demo of sending and receiving some data.
-  Be sure to check out other examples!
- *************************************************************/
-
-/* Fill-in information from Blynk Device Info here */
 #define BLYNK_TEMPLATE_ID           "TMPL6TjG8TTD6"
 #define BLYNK_TEMPLATE_NAME         "Quickstart Template"
 #define BLYNK_AUTH_TOKEN            "_7ohK1KQMrkYLhzUZv2ly9uTV-7N9lx4"
 
-/* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
 
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
 char ssid[] = "Hotspot_ko";
 char pass[] = "abcdefghij";
 
 BlynkTimer timer;
 
-// This function is called every time the Virtual Pin 0 state changes
 BLYNK_WRITE(V0)
 {
-  // Set incoming value from pin V0 to a variable
   int value = param.asInt();
 
-  // Update state
   Blynk.virtualWrite(V1, value);
 }
 
-// This function is called every time the device is connected to the Blynk.Cloud
+
 BLYNK_CONNECTED()
 {
-  // Change Web Link Button message to "Congratulations!"
-  Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
-  Blynk.setProperty(V3, "onImageUrl",  "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
-  Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
+  Serial.println("Connected to blynk!");
 }
 
-// This function sends Arduino's uptime every second to Virtual Pin 2.
 void myTimerEvent()
 {
   // You can send any value at any time.
