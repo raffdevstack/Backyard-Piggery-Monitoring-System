fba6887eaee2d515726399f74bd99589a731135c
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2024-12-16 14:53:01 +0800

    done functional wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,19 @@ const char* pass = "abcdefghij";
 
 void setup()
 {
-  
+    Serial.begin(115200);
+    delay(10);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        WiFi.begin(ssid, pass);
+        Serial.print("connecting ");
+        delay(5000);
+    }
+
+    Serial.println();
+    Serial.println("Connected to WiFi!");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop()
