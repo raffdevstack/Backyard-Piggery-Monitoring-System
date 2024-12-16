f33a1044ab20aa5d778b721c8c976ce24751ccac
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2024-12-16 15:26:24 +0800

    combine blynk to wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,52 @@
 
+#define BLYNK_TEMPLATE_ID "TMPL6lDFeOAuC"
+#define BLYNK_TEMPLATE_NAME "Backyard Piggery"
+#define BLYNK_AUTH_TOKEN "a4MdgoRqs5ljj7yhqHRd9aOUdIyKAk6j"
+
+#define BLYNK_PRINT Serial
+
 #include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 
 const char* ssid = "Hotspot_ko";
 const char* pass = "abcdefghij";
 
-void setup()
-{
-    Serial.begin(115200);
-    delay(10);
+BlynkTimer timer;
 
+BLYNK_CONNECTED()
+{
+    Serial.println("CONNECTED TO BLYNK");
+}
+
+void myTimerEvent()
+{
+    Serial.println("blynk timer works");
+}
+
+void connectToWifi() {
     while (WiFi.status() != WL_CONNECTED) {
         WiFi.begin(ssid, pass);
         Serial.print("connecting ");
         delay(5000);
     }
-
     Serial.println();
     Serial.println("Connected to WiFi!");
     Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());
 }
 
+void setup()
+{
+    Serial.begin(115200);
+    delay(10);
+
+    
+
+    timer.setInterval(1000L, myTimerEvent);
+}
+
 void loop()
 {
-    
+    Blynk.run();
+    timer.run();
 }
