bba1657e1f90fb30ac7d0b1ffa18525e0da30e1c
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2024-12-16 14:37:25 +0800

    focus on wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,15 @@
 
-#define BLYNK_TEMPLATE_ID           "TMPL6TjG8TTD6"
-#define BLYNK_TEMPLATE_NAME         "Quickstart Template"
-#define BLYNK_AUTH_TOKEN            "_7ohK1KQMrkYLhzUZv2ly9uTV-7N9lx4"
-
-#define BLYNK_PRINT Serial
-
-#include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
 
-char ssid[] = "Hotspot_ko";
-char pass[] = "abcdefghij";
-
-BlynkTimer timer;
-
-BLYNK_WRITE(V0)
-{
-  int value = param.asInt();
-
-  Blynk.virtualWrite(V1, value);
-}
-
-
-BLYNK_CONNECTED()
-{
-  Serial.println("Connected to blynk!");
-}
-
-void myTimerEvent()
-{
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V2, millis() / 1000);
-}
+const char* ssid = "Hotspot_ko";
+const char* pass = "abcdefghij";
 
 void setup()
 {
-  // Debug console
-  Serial.begin(115200);
-
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-  // You can also specify server:
-  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
-  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, IPAddress(192,168,1,100), 8080);
-
-  // Setup a function to be called every second
-  timer.setInterval(1000L, myTimerEvent);
+  
 }
 
 void loop()
 {
-  Blynk.run();
-  timer.run();
-  // You can inject your own code or combine it with other sketches.
-  // Check other examples on how to communicate with Blynk. Remember
-  // to avoid delay() function!
+    
 }
